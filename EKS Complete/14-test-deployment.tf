d73fbb6b6673905e66bd367b079d4e50402bb331
Harshvardhan Singh Jadon <jadonharsh109@gmail.com>
2024-01-14 16:42:00 +0530

    bug fix

--- EKS Complete/14-test-deployment.tf
+++ EKS Complete/14-test-deployment.tf
@@ -1,8 +1,8 @@
 // This will deploy custom helm charts for testing purpose.
 resource "helm_release" "my-release" {
-  name      = "test-deployment"
-  chart     = "k8s"     # Local path to the chart
-  namespace = "default" # Optional: Specify namespace
-
+  name       = "test-deployment"
+  chart      = "k8s"     # Local path to the chart
+  namespace  = "default" # Optional: Specify namespace
+  timeout    = 600
   depends_on = [helm_release.aws-load-balancer-controller]
 }
